c287ab4a9f4ec65e70c7459d50f87a3ab62e5df3
alain-x <alainvalentin04@gmail.com>
2026-06-08 10:04:29 +0200

    initial commit

--- src/main/java/com/online/attendance/sports/evaluation/PlayerEvaluationRepository.java
+++ src/main/java/com/online/attendance/sports/evaluation/PlayerEvaluationRepository.java
@@ -4,12 +4,17 @@ import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface PlayerEvaluationRepository extends JpaRepository<PlayerEvaluation, Long> {
 
     @EntityGraph(attributePaths = {"player", "player.user", "evaluator", "team"})
     @Override
     List<PlayerEvaluation> findAll();
+
+    @EntityGraph(attributePaths = {"player", "player.user", "evaluator", "team"})
+    @Override
+    Optional<PlayerEvaluation> findById(Long id);
 
     @EntityGraph(attributePaths = {"player", "player.user", "evaluator", "team"})
     List<PlayerEvaluation> findByPlayerId(Long playerId);
